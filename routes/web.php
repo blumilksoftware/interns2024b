aea9b4b9084fe0d1e3d5dedfac65ec6bb5f6378f
Mykyta Mykulskyi <anonymous@email.com>
2024-08-12 09:11:03 +0200

    fix: changed "/home" path to "/"

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Inertia\Response;
 
-Route::get("/", fn(): Response => inertia("Welcome"));
-Route::get("/home", fn(): Response => inertia("Home"));
+Route::get("/", fn(): Response => inertia("Home"));
