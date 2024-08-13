ac652575058735f6724b7862fda9f97ef24287fb
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-08-13 07:47:57 +0200

    Add .vue for Forgot-Password

--- resources/js/Pages/Auth/Forgot-Password.vue
+++ resources/js/Pages/Auth/Forgot-Password.vue
@@ -4,11 +4,11 @@ import { router } from '@inertiajs/vue3'
 
 const form = reactive({
   email: null,
-  password: null,
+
 })
 
 function submit() {
-  router.post('/auth/login', form)
+  router.post('/auth/forget-password', form)
 }
 </script>
 
@@ -16,10 +16,6 @@ function submit() {
   <form @submit.prevent="submit">
     <label for="email">Email:</label>
     <input v-model="form.email" name="email" type="email">
-    <label for="password">password:</label>
-    <input v-model="form.password" name="password" type="password">
-    <button type="submit">Login</button>
+    <button type="submit">Reset?</button>
   </form>
-
-  <a href="/auth/forgot-password">I forgot the password :(</a>
 </template>
