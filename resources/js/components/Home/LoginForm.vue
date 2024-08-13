1f0e25b14d32758c144879f671a70f89763ac625
Mykyta Mykulskyi <81817136+JokeUrSelf@users.noreply.github.com>
2024-08-13 14:10:59 +0200

    rephrasing and removing a line

--- resources/js/components/Home/LoginForm.vue
+++ resources/js/components/Home/LoginForm.vue
@@ -29,10 +29,9 @@
               focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary
             "
       >
-        Continue
+        Zaloguj się
       </button>
     </div>
   </form>
 </template>
-
 
