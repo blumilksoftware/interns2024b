43f7945448a59a3b99b65237d2a1bcb396c04442
Mykyta Mykulskyi <anonymous@email.com>
2024-08-14 11:43:32 +0200

    fix: incorrect banner text color with a dark mode turned on

--- resources/js/components/Home/Footer.vue
+++ resources/js/components/Home/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="bg-white shadow">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-200">© Collegium Witelona: Testy</span>
-      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-200 sm:mt-0">
+      <span class="text-sm text-gray-500 sm:text-center">© Collegium Witelona: Testy</span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
         <li>
           <a href="#" class="hover:underline me-4 md:me-6">Polityka prywatności</a>
         </li>
