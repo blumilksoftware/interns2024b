6d0987f78be4ba5deba8aa9f675ee1e6ea075455
AmonDeShir <amonsir29@gmail.com>
2024-08-19 09:03:47 +0200

    rename canBeLocked to isReadyToBePublished

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -46,12 +46,12 @@ class Quiz extends Model
 
     public function isLocked(): Attribute
     {
-        return Attribute::get(fn(): bool => $this->canBeLocked() && $this->scheduled_at <= Carbon::now());
+        return Attribute::get(fn(): bool => $this->isReadyToBePublished() && $this->scheduled_at <= Carbon::now());
     }
 
     public function closeAt(): Attribute
     {
-        return Attribute::get(fn(): ?Carbon => $this->canBeLocked() ? $this->scheduled_at->copy()->addSeconds($this->duration) : null);
+        return Attribute::get(fn(): ?Carbon => $this->isReadyToBePublished() ? $this->scheduled_at->copy()->addSeconds($this->duration) : null);
     }
 
     public function clone(): self
@@ -84,7 +84,7 @@ class Quiz extends Model
         return $submission;
     }
 
-    protected function canBeLocked(): bool
+    protected function isReadyToBePublished(): bool
     {
         return $this->scheduled_at !== null && $this->duration !== null;
     }
