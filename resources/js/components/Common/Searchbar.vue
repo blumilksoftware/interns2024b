51d46c8ef25e94e63e794b1332383ef1be0ea305
Mykyta Mykulskyi <anonymous@email.com>
2024-08-19 09:01:18 +0200

    style: added icon to indicate searchbar

--- resources/js/components/Common/Searchbar.vue
+++ resources/js/components/Common/Searchbar.vue
@@ -20,16 +20,25 @@ const onOptionClick = (obj:any)=>{
 
 <template>
   <div :class="{'scale-y-100 max-h-80': isSearchFocused}" class="overflow-hidden duration-200 max-h-12 flex flex-col mt-2 bg-white/30 placeholder:text-gray-400 rounded-[.5rem] ring-2 ring-primary/30 ring-inset">
-    <input
-      :value="isSearchFocused ? searchQuery : selectedOption"
-      class="outline-none focus:duration-200 focus:ring-inset focus:ring focus:ring-primary rounded-[.5rem] p-3 bg-transparent w-full text-gray-900"
-      required name="search-schools" :class="{'cursor-pointer' : !isSearchFocused}"
-      type="text"
-      :placeholder="selectedOption"
-      @input="(e:any)=>searchQuery=e.target.value"
-      @focus="isSearchFocused=true"
-      @blur="isSearchFocused=false"
-    >
+    <div class="flex h-inherit items-center justify-center duration-200 rounded-[.5rem]" :class="{'ring-inset ring ring-primary':isSearchFocused}">
+      <div class="h-full items-center justify-center px-3">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 stroke-primary/50">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+      </div>
+
+      <input
+        :value="isSearchFocused ? searchQuery : selectedOption"
+        class="outline-none py-3 bg-transparent w-full text-gray-900"
+        required name="search-schools" :class="{'cursor-pointer' : !isSearchFocused}"
+        type="text"
+        :placeholder="selectedOption"
+        @input="(e:any)=>searchQuery=e.target.value"
+        @focus="isSearchFocused=true"
+        @blur="isSearchFocused=false"
+      >
+    </div>
+
     <Transition>
       <div v-show="true" class="m-0.5 mt-0 py-2 overflow-auto">
         <div v-if="filteredSchools.length>0">
