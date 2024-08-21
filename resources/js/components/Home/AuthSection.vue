ff1a80f1bc3ce8210fa379f06b23175f5e19dc56
Mykyta Mykulskyi <anonymous@email.com>
2024-08-21 14:40:17 +0200

    refactor: remove imports of macros

--- resources/js/components/Home/AuthSection.vue
+++ resources/js/components/Home/AuthSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, defineExpose } from 'vue'
+import { ref } from 'vue'
 import AuthButton from '@/components/Common/AuthButton.vue'
 import RegisterForm from '@/components/Home/RegisterForm.vue'
 import LoginForm from '@/components/Home/LoginForm.vue'
