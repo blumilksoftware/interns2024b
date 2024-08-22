f08652885783c6f76d56245905d33c1fdf7d30f0
Dominikaninn <130690231+PrabuckiDominik@users.noreply.github.com>
2024-08-22 12:35:20 +0200

    Apply suggestions from code review
    Co-authored-by: Ewelina Skrzypacz <56546832+EwelinaSkrzypacz@users.noreply.github.com>

--- resources/js/Pages/Auth/PasswordUpdate.vue
+++ resources/js/Pages/Auth/PasswordUpdate.vue
@@ -27,16 +27,16 @@ function updatePassword() {
 <template>
   <div>
     <form @submit.prevent="updatePassword">
-      <label for="current_password">Aktualne Hasło:</label>
+      <label for="current_password">Aktualne hasło:</label>
       <input v-model="form.current_password" name="password" type="password" required><br>
       <div v-if="errors.current_password">{{ props.errors.current_password }}</div>
-      <label for="password">Nowe Hasło:</label>
+      <label for="password">Nowe hasło:</label>
       <input v-model="form.password" name="password" type="password"><br>
       <div v-if="errors.password">{{ props.errors.password }}</div>
-      <label for="password_confirmation">Potwierdzenie Nowego Hasła:</label>
+      <label for="password_confirmation">Potwierdzenie nowego hasła:</label>
       <input v-model="form.password_confirmation" name="password_confirmation" type="password"><br>
       <div v-if="errors.password_confirmation">{{ props.errors.password_confirmation }}</div>
-      <button type="submit">Utwórz</button>
+        <button type="submit">Zmień hasło</button>
     </form>
   </div>
 </template>
