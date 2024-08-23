cadee2f09e0e99aa0e82f7d5031728ba9edef9f4
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-08-23 10:49:09 +0200

    Add request for profile user while reseting password

--- app/Http/Controllers/ProfileUserController.php
+++ app/Http/Controllers/ProfileUserController.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\ProfileUserPasswordResetRequest;
 use App\Http\Resources\UserResource;
 use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rules\Password;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -32,12 +32,9 @@ class ProfileUserController extends Controller
         return Inertia::render("Auth/PasswordUpdate");
     }
 
-    public function update(Request $request, Hasher $hasher): RedirectResponse
+    public function update(ProfileUserPasswordResetRequest $request, Hasher $hasher): RedirectResponse
     {
-        $validated = $request->validate([
-            "current_password" => ["required", "current_password"],
-            "password" => ["required", Password::defaults(), "confirmed"],
-        ]);
+        $validated = $request->validated();
 
         $user = $request->user();
         $user->password = $hasher->make($validated["password"]);
