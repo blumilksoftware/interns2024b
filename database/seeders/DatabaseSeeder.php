c24dcc41bc210306779033c42570579d8981bf4c
Mykyta Mykulskyi <anonymous@email.com>
2024-08-23 10:12:55 +0200

    Merge branch 'main' of https://github.com/blumilksoftware/interns2024b into 26-admin-panel-for-managing-tests-frontend update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\Answer;
 use App\Models\AnswerRecord;
+use App\Models\Question;
 use App\Models\Quiz;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -15,8 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create();
-        Quiz::factory()->create();
-        Answer::factory()->create();
+        $quiz = Quiz::factory()->create();
+
+        $questions = Question::factory()->count(10)->create(["quiz_id" => $quiz->id]);
+
+        foreach ($questions as $question) {
+            Answer::factory()->count(4)->create(["question_id" => $question->id]);
+        }
+
         AnswerRecord::factory()->create();
     }
 }
