eaeb7dc231bfa9f7c45935dfc9887734497afbf2
Dominik Purgał <amonsir29@gmail.com>
2024-08-27 09:48:08 +0200

    #28 - CRUD for schools backend (#34)
    * Add migration for schools_table and modify users_table
    
    * Add migration for schools_table and modify users_table
    
    * Add:
    - register and login forms
    - RegisterUserController and RegisterUserRequest
    - ContestController
    - improve user model
    - add foreign key to user table
    
    * Fix code style
    
    * Add tests and SchoolFactory
    
    * Mail Verification
    
    * Test Mail
    
    * Add EmailVerifyController
    Add PasswordResetLinkController
    
    * Improve DatabaseSeeder and Factories
    
    * Fix RegisterUserController
    Add and improve tests
    
    * Increase bcrypt round to 12
    
    * Add .vue for Forgot-Password
    
    * Code style fix
    
    * Add reset password
    
    * Add tests for reset password
    
    * fix code style
    
    * Add validation errors and messages
    Add:
     validation errors and messages
    Improve:
     Controllers with requests
     Tests
    
    * Add validation translation files
    Change to pl language in .env.example.
    Change some files name into PascalCase.
    Remove unused verify-email.
    Remove messages from requests.
    
    * Fix email error message for existing email addresses
    
    * Link frontend with backend
    Add SchoolResource
    Remove unused route and pages
    
    * Pack routes into group middleware
    
    * Remove empty spaces
    
    * Rename zipCode to zip_code
    
    * Remove app_locale from .env.example
    
    * Move some logic into if in RegisterUserController
    
    * Remove dead code
    
    * Fix code style
    
    * Change redirect to home after password reset
    
    * Update redirect path in ResetPasswordTest
    
    * Fix return message in forgot password
    
    * Add:
    - Schools are sortable by name in searchbar in register form
    
    * Remove : APP_FALLBACK_LOCALE and APP_FAKER_LOCALE from .env.example
    
    * Add attribute required to checkbox
    
    * implement school fetching from the RSPO api
    
    * Fix searchbar
    
    * Add CRUD for schools
    
    * add tests for fetch and status routes
    
    * test GetSchoolDataService
    
    * fix answer record tests
    
    * Update config/saloon.php
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * Change sneak_case to camelCase
    
    * translate status texts to Polish
    
    * fix auth test
    
    * fix school tests
    
    * Update app/Http/Integrations/RSPOConnector/RSPOConnector.php
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * Update app/Http/Integrations/RSPOConnector/RSPOConnector.php
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * fix code style
    
    * Update app/Http/Integrations/RSPOConnector/Requests/GetSchoolsRequest.php
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * Update app/Http/Integrations/RSPOConnector/Requests/GetSchoolsRequest.php
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * fix code style
    
    * Move voivodeships to config
    
    * convert SchoolType and Voivodeship to enums
    
    ---------
    
    Co-authored-by: Dominik Prabucki <dominikprabucki.2002@gmail.com>
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>

---
+++ app/Http/Integrations/RSPOConnector/RSPOConnector.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Integrations\RSPOConnector;
+
+use Saloon\Http\Connector;
+use Saloon\Http\Request;
+use Saloon\PaginationPlugin\Contracts\HasPagination;
+use Saloon\PaginationPlugin\Paginator;
+use Saloon\Traits\Plugins\AcceptsJson;
+
+class RSPOConnector extends Connector implements HasPagination
+{
+    use AcceptsJson;
+
+    public function resolveBaseUrl(): string
+    {
+        return "https://api-rspo.mein.gov.pl/api";
+    }
+
+    public function paginate(Request $request): Paginator
+    {
+        return new RSPOPaginator($this, $request);
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            "Accept" => "application/ld+json",
+        ];
+    }
+}
