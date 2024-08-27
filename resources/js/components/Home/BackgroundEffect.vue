c57424f44dd804485febd2623edca4c59cbb800d
Mykyta Mykulskyi <anonymous@email.com>
2024-08-27 14:12:24 +0200

    fix: remove a line from backgfround effect

--- resources/js/components/Home/BackgroundEffect.vue
+++ resources/js/components/Home/BackgroundEffect.vue
@@ -23,5 +23,4 @@ onUnmounted(() => window.removeEventListener('scroll', setBannerVisibility))
       );"
     />
   </div>
-  <div class="inset-0 top-16  h-[22rem] lg:h-screen lg:top-0 bg-white/30 absolute -z-10" />
 </template>
