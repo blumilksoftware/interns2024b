32878093c5ea0aaf4504d7730beb423fc70bd98b
Mykyta Mykulskyi <anonymous@email.com>
2024-08-27 14:52:40 +0200

    feat: profil

--- resources/js/Pages/Auth/Profile.vue
+++ resources/js/Pages/Auth/Profile.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import { useForm } from '@inertiajs/vue3'
+import BackgroundEffect from '@/components/Home/BackgroundEffect.vue'
+import Footer from '@/components/Common/Footer.vue'
 
 const props = defineProps<{
   user: {
@@ -23,14 +25,33 @@ function submit() {
 </script>
 
 <template>
-  <div>
-    <p>Imię: {{ props.user.name }}</p>
-    <p>Nazwisko: {{ props.user.surname }}</p>
-    <p>E-mail: {{ props.user.email }}</p>
-    <p>Szkoła: {{ props.user.school.name }}</p>
-    <form @submit.prevent="submit">
-      <button type="submit">Formularz zmiany hasła</button>
-    </form>
-    <div v-if="status" class="alert alert-success">{{ status }}</div>
+  <BackgroundEffect />
+  <div class="inset-0 top-16 bg-white/30 absolute -z-10" />
+  <div class="flex flex-col items-center pt-12 h-screen">
+    <div class="flex flex-col gap-4">
+      <div class="grid grid-cols-2 gap-4">
+        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+          <b>Imię:</b> 
+          <p>{{ props.user.name }}</p>
+        </div>
+        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+          <b>Nazwisko:</b> 
+          <p>{{ props.user.surname }}</p>
+        </div>
+        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+          <b>E-mail:</b> 
+          <p>{{ props.user.email }}</p>
+        </div>
+        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+          <b>Szkoła:</b> 
+          <p>{{ props.user.school.name }}</p>
+        </div>
+      </div>
+      <form @submit.prevent="submit">
+        <button class="bg-primary text-white font-bold py-3 px-4 rounded-lg w-full" type="submit">Zmień hasło</button>
+      </form>
+      <div v-if="status" class="alert alert-success">{{ status }}</div>
+    </div>
+    <Footer />
   </div>
 </template>
