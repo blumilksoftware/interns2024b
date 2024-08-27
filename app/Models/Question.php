af0ffb2eaf02405881e53d262f336cc6c8f2c2cf
Amon De Shir <amonsir29@gmail.com>
2024-08-27 09:13:43 +0200

    fix hasCorrectAnswer method

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -55,7 +55,7 @@ class Question extends Model
 
     public function hasCorrectAnswer(): Attribute
     {
-        return Attribute::get(fn(): bool => $this->correct_answer_id === null);
+        return Attribute::get(fn(): bool => $this->correct_answer_id !== null);
     }
 
     public function cloneTo(Quiz $quiz): self
