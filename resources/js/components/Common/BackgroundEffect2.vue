98dd0e9a3351ac24badd1c3b31a9eeb5458a30f6
Mykyta Mykulskyi <anonymous@email.com>
2024-08-27 19:22:25 +0200

    feat: background effect 2

---
+++ resources/js/components/Common/BackgroundEffect2.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="absolute opacity-50 -z-50">
+    <BackgroundEffect />
+  </div>
+  <div class="bg-primary-600/10 size-full fixed top-0 left-0 -z-40" />
+</template>
