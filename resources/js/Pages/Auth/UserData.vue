be8cba840e2cf1a112f22a162b76f8fa74f04295
Mykyta Mykulskyi <anonymous@email.com>
2024-08-28 11:46:49 +0200

    feat: combine profile page and password edit

--- resources/js/Pages/Auth/UserData.vue
+++ resources/js/Pages/Auth/UserData.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
   user: {
     name: string
     surname: string
@@ -8,8 +8,6 @@ const props = defineProps<{
       name: string
       }
     }
-  errors: Record<string, string[]>
-  status?: string
   }>()
 </script>
 
@@ -17,19 +15,19 @@ const props = defineProps<{
   <div class="flex flex-col w-full sm:grid sm:grid-cols-2 gap-4">
     <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
       <b>Imię:</b> 
-      <p>{{ props.user.name }}</p>
+      <p>{{ user.name }}</p>
     </div>
     <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
       <b>Nazwisko:</b> 
-      <p>{{ props.user.surname }}</p>
+      <p>{{ user.surname }}</p>
     </div>
     <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
       <b>E-mail:</b> 
-      <p>{{ props.user.email }}</p>
+      <p>{{ user.email }}</p>
     </div>
     <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
       <b>Szkoła:</b> 
-      <p>{{ props.user.school.name }}</p>
+      <p>{{ user.school.name }}</p>
     </div>
   </div>
 </template>
--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -6,7 +6,7 @@ import Header from '@/components/Common/Header.vue'
 import PasswordUpdate from '@/Pages/Auth/PasswordUpdate.vue'
 import UserData from '@/Pages/Auth/UserData.vue'
 
-const props = defineProps<{
+defineProps<{
   user: {
     name: string
     surname: string
@@ -23,15 +23,15 @@ const props = defineProps<{
 
 <template>
   <BackgroundEffect />
-  <div class="inset-0 bg-white/30 absolute -z-10" />
+  <div class="inset-0 bg-white/70 absolute -z-10" />
   <div class="flex flex-col items-center h-screen">
     <Header title="Profil" />
     <div class="flex flex-col gap-4 p-5 w-full sm:w-fit">
       <b class="text[1.1rem]">Twoje dane:</b>
-      <UserData {{...props}} />
+      <UserData :user="user" />
       <hr class="border-primary/30 m-5">
       <PasswordUpdate :errors="errors" />
-      <div v-if="status" class="alert alert-success">{{ status }}</div>
+      <div v-if="true" class="alert alert-success">{{ 'status' }}</div>
     </div>
     <Footer />
   </div>
