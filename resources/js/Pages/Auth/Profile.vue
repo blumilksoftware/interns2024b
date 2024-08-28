1ff434aa9237aa18f01dd913243b2be69e1f8519
Mykyta Mykulskyi <anonymous@email.com>
2024-08-28 08:49:09 +0200

    feat: responsivness

--- resources/js/Pages/Auth/Profile.vue
+++ resources/js/Pages/Auth/Profile.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
-import { useForm } from '@inertiajs/vue3'
 import BackgroundEffect from '@/components/Home/BackgroundEffect.vue'
 import Footer from '@/components/Common/Footer.vue'
+import Header from '@/components/Common/Header.vue'
+import { useForm } from '@inertiajs/vue3'
 
 const props = defineProps<{
   user: {
@@ -26,27 +27,29 @@ function submit() {
 
 <template>
   <BackgroundEffect />
-  <div class="inset-0 top-16 bg-white/30 absolute -z-10" />
-  <div class="flex flex-col items-center pt-12 h-screen">
-    <div class="flex flex-col gap-4">
-      <div class="grid grid-cols-2 gap-4">
-        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+  <div class="inset-0 bg-white/30 absolute -z-10" />
+  <div class="flex flex-col items-center h-screen">
+    <Header title="Profil" />
+    <div class="flex flex-col gap-4 p-5 w-full">
+      <div class="flex flex-col w-full sm:grid sm:grid-cols-2 gap-4">
+        <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
           <b>Imię:</b> 
           <p>{{ props.user.name }}</p>
         </div>
-        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+        <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
           <b>Nazwisko:</b> 
           <p>{{ props.user.surname }}</p>
         </div>
-        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+        <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
           <b>E-mail:</b> 
           <p>{{ props.user.email }}</p>
         </div>
-        <div class="border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
+        <div class="w-full border border-primary/30 py-2 px-4 rounded-lg bg-white/30">
           <b>Szkoła:</b> 
           <p>{{ props.user.school.name }}</p>
         </div>
       </div>
+      <hr class="border-primary/30 mx-5">
       <form @submit.prevent="submit">
         <button class="bg-primary text-white font-bold py-3 px-4 rounded-lg w-full" type="submit">Zmień hasło</button>
       </form>
