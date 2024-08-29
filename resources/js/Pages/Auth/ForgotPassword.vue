1eb64e6166f56d3b4eba05c7674a25d7444e0575
Mykyta Mykulskyi <anonymous@email.com>
2024-08-29 12:09:04 +0200

    feat: added foooter

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -3,7 +3,7 @@ import { router } from '@inertiajs/vue3'
 import CustomInput from '@/components/Common/CustomInput.vue'
 import { ref } from 'vue'
 import Banner from '@/components/Common/Banner.vue'
-import BackgroundEffect from '@/components/Home/BackgroundEffect.vue'
+import Footer from '@/components/Home/Footer.vue'
 
 defineProps<{
   errors: Record<string, string>
@@ -18,12 +18,14 @@ function submit() {
 </script>
 
 <template>
-  <BackgroundEffect />
-  <Banner :content="status" />
-  <form class="mt-8 p-5 gap-5 flex flex-col" @submit.prevent="submit">
-    <CustomInput v-model="email" label="E-mail" :error="errors.email" name="email" type="email" />
-    <button type="submit" class="bg-primary text-white font-bold py-3 px-4 rounded-lg">
-      Zmień hasło
-    </button>
-  </form>
+  <div class="flex flex-col items-center h-screen w-full">
+    <Banner :content="status" />
+    <form class="mt-8 p-5 gap-5 flex flex-col w-full" @submit.prevent="submit">
+      <CustomInput v-model="email" label="E-mail" :error="errors.email" name="email" type="email" />
+      <button type="submit" class="bg-primary text-white font-bold py-3 px-4 rounded-lg">
+        Zmień hasło
+      </button>
+    </form>
+    <Footer />
+  </div>
 </template>
