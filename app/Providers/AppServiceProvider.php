67b55239daa8d65a4233eb5c5401ad103501fb79
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-08-29 12:57:52 +0200

    Add resend verification mail and must verify mail to access user dashboard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,5 +16,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
     }
 }
---
+++ resources/js/Pages/Auth/Verify-Email.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {useForm} from '@inertiajs/vue3'
+const form = useForm({})
+
+function logout() {
+  form.get('/auth/logout')
+}
+
+function sent() {
+  form.post('/email/verification-notification')
+}
+
+</script>
+
+<template>
+  <form @submit.prevent="sent">
+    <button type="submit">Wyślij ponownie link weryfikacyjny</button>
+  </form>
+
+  <form @submit.prevent="logout">
+    <button type="submit">Logout</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
