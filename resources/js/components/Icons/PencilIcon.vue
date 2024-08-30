8212e9ae954aca36693f3d6fd11c52a71866aa7c
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-08-30 18:52:50 +0200

    feat: icons

--- resources/js/components/Icons/PencilIcon.vue
+++ resources/js/components/Icons/PencilIcon.vue
@@ -1,7 +1,3 @@
-<script setup>
-import IconLayout from '@/Layouts/IconLayout.vue'
-</script>
-
 <template>
   <IconLayout>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6 text-primary">
