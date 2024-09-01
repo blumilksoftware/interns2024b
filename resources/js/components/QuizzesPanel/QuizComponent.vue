ecdab9ac013d466e03bd761a1cdfee7fe229d3fd
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-09-01 16:42:30 +0200

    refactor

--- resources/js/components/QuizzesPanel/QuizComponent.vue
+++ resources/js/components/QuizzesPanel/QuizComponent.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref, watch } from 'vue'
 import axios from 'axios'
 import { router, useForm } from '@inertiajs/vue3'
 import dayjs from 'dayjs'
@@ -19,12 +19,31 @@ const props = defineProps<{quiz:Quiz, isSelected:boolean, showLockedQuizzes:bool
 const emit = defineEmits(['displayToggle'])
 const isRequestOngoing = ref<boolean>(false)
 const isEditing = ref<boolean>(false)
-const quizRef = computed<Quiz>(()=>props.quiz)
+const quizRef = ref<Quiz>(props.quiz)
 
-function onStartEditing(){ 
-  isEditing.value=!isEditing.value
+watch(
+  () => props.quiz,
+  (updatedQuiz : Quiz) => quizRef.value = updatedQuiz,
+)
+
+function startEditing(){
+  isEditing.value=true
   if (!props.isSelected)
     toggleQuizView()
+}
+
+function dismissEditing(){
+  isEditing.value=false
+  quizRef.value = props.quiz
+}
+
+function saveEditing(){
+  isEditing.value=false
+
+}
+
+function publish(){
+
 }
 
 async function request(url : string, method: 'POST' | 'PATCH' | 'DELETE', payload? : object) {
@@ -94,13 +113,13 @@ function toggleQuizView() {
       <span v-if="!isSelected">Czas rozpoczęcia: <b class="whitespace-nowrap">{{ formatDatePretty(quiz.scheduledAt) ? formatDatePretty(quiz.scheduledAt) : 'brak' }}</b> </span>
       <span v-if="!isSelected">Czas trwania: <b class="whitespace-nowrap">{{ quiz.duration ? quiz.duration + ' minut': "brak" }}</b> </span>
       <div class="flex gap-5 justify-end">
-        <button v-if="!isEditing && !quiz.locked" class="bg-primary rounded-lg py-2 px-4 text-white font-bold">Opublikuj</button>
-        <button v-if="!quiz.locked && !isEditing" title="Edytuj test" @click="onStartEditing"><PencilIcon /></button>
-        <button v-if="!quiz.locked && isEditing" title="Anuluj edytowanie testu" @click="onStartEditing"><DismissIcon /></button>
-        <button v-if="!quiz.locked && isEditing" title="Zapisz edytowany test" @click="onStartEditing"><CheckIcon /></button>
+        <button v-if="!quiz.locked && !isEditing" title="Edytuj test" @click="startEditing"><PencilIcon /></button>
+        <button v-if="!quiz.locked && isEditing" title="Anuluj edytowanie testu" @click="dismissEditing"><DismissIcon /></button>
+        <button v-if="!quiz.locked && isEditing" title="Zapisz edytowany test" @click="saveEditing"><CheckIcon /></button>
         <button v-if="!isEditing" title="Skopiuj test" @click="copyQuiz(quiz.id)"><CopyIcon /></button>
         <div v-if="quiz.locked" title="Ten test jest zablokowany. Nie można go modyfikować ani usunąć"><LockIcon /></div>
         <button v-else title="Usuń test" @click="deleteQuiz(quiz.id)"><TrashIcon /></button>
+        <button v-if="!isEditing && !quiz.locked" class="bg-primary rounded-lg py-2 px-4 text-white font-bold" @click="publish">Opublikuj</button>
       </div>
     </div>
     <!-- header/ -->
@@ -110,8 +129,8 @@ function toggleQuizView() {
       <div class="grid grid-cols-[auto,auto] gap-2 w-fit rounded-lg items-center">
         <span>Rozpoczęcie testu:</span>
         <EditableInput type="datetime-local" :is-editing="isEditing" :v-model:value="formatDateHTML(quiz.scheduledAt)" />
-        <span>Zakończenie testu:</span>
-        <EditableInput type="datetime-local" :is-editing="isEditing" :v-model="quiz.duration" />
+        <span>Czas trwania testu:</span>
+        <EditableInput placeholder="Podaj czas w minutach" type="number" min="0" :is-editing="isEditing" :v-model="quiz.duration" />
       </div>
           
       <!-- question -->
@@ -121,8 +140,8 @@ function toggleQuizView() {
         </div>
 
         <data v-if="isSelected" class="flex flex-col gap-4">
-          <div v-for="question of quiz.questions" :key="question.id">
-            <QuestionComponent :quiz-id="quiz.id" :question="question" />
+          <div v-for="(question, idx) of quizRef.questions" :key="question.id">
+            <QuestionComponent v-model="quizRef.questions[idx]" :is-editing="isEditing" :quiz-id="quiz.id" />
           </div>
         </data>
       </div>
