1a842ca99f8496c5f88c909d66e42eed2ea37a82
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-09-02 11:10:21 +0200

    chore: reimplement answers

--- resources/js/components/QuizzesPanel/QuestionComponent.vue
+++ resources/js/components/QuizzesPanel/QuestionComponent.vue
@@ -5,35 +5,48 @@ import CopyIcon from '@/components/Icons/CopyIcon.vue'
 import CheckDynamicIcon from '@/components/Icons/CheckDynamicIcon.vue'
 
 import { computed, ref } from 'vue'
-import { router } from '@inertiajs/vue3'
 import { type Question } from '@/Types/Question'
+import { type CleanAnswer } from '@/Types/CleanAnswer'
 
-const props = defineProps<{ quizId:number }>()
+defineProps<{ quizId:number }>()
+const emit = defineEmits<{delete:[questionId: number]}>()
 const question = defineModel<Question>({required: true})
 const isAnswerExpanded = ref<boolean>(false)
 const hasAnswers = computed(()=>question.value.answers.length > 0)
 
 function deleteQuestion() {
-  router.delete(`/admin/questions/${question.value.id}`)
+  emit('delete', question.value.id)
 }
 
 function copyQuestion() {
-  router.post(`/admin/questions/${question.value.id}/clone/${props.quizId}`)
 }
 
 function addAnswer(){
-  router.post(`/admin/questions/${question.value.id}/answers`)
+  const answer : CleanAnswer = {
+    text: 'Nowa odpowiedź',
+    correct: false,
+  }
+  question.value.answers.push(answer)
+  isAnswerExpanded.value = true
+}
+function deleteAnswer(answer:CleanAnswer){
+  question.value.answers = question.value.answers.filter(ans => ans !== answer)
+}
+function setCorrectAnswer(answer: CleanAnswer) {
+  for (const ans of question.value.answers)
+    ans.correct = false
+  answer.correct = true
 }
 </script>
 
 <template>
   <div class="rounded-lg border border-primary/30 flex justify-between">
-    <div class="flex flex-col gap-4 p-4">
+    <div class="flex flex-col gap-4 p-4 w-full">
       <div class="flex flex-col gap-1">
         <b class="text-[1.1rem]">Pytanie {{ question.id }}</b>
         <p>{{ question.text }}</p>
       </div>
-      <div class="flex gap-4">
+      <div class="flex gap-4 ">
         <button class="w-fit border border-primary/30 rounded-lg py-2 px-3 gap-2 flex bg-white/50" @click="addAnswer">
           <b>+ Dodaj odpowiedź</b>
         </button>
@@ -42,10 +55,15 @@ function addAnswer(){
           <ExapnsionToggleDynamicIcon :is-expanded="isAnswerExpanded" />
         </button>
       </div>
-      <ol v-if="isAnswerExpanded && hasAnswers" class="flex flex-col gap-4">
-        <li v-for="answer in question.answers" :key="answer.id" class="border border-primary/30 rounded-lg p-4 bg-white/50 flex items-center gap-4">
-          <CheckDynamicIcon class="size-6" :is-correct="question.correct==answer.id" />
-          {{ answer.text }}
+      <ol v-if="isAnswerExpanded && hasAnswers" class="flex flex-col gap-4 w-full">
+        <li v-for="answer of question.answers" :key="answer.id" class="w-full flex border border-primary/30 rounded-lg bg-white/50">
+          <button class="p-4 flex items-center gap-4 w-full" @click="setCorrectAnswer(answer)">
+            <CheckDynamicIcon class="size-6" :is-correct="answer.correct" />
+            {{ answer.text }}
+          </button>
+          <div class="px-3 border-l border-primary/30 flex flex-col justify-evenly">
+            <button data-name="delete" @click="deleteAnswer(answer)"><TrashIcon /></button>
+          </div>
         </li>
       </ol>
     </div>
