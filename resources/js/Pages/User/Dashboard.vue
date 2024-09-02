74f2d2879cd9adfaddbb1e8d682e5c8c7b95b135
AmonDeShir <amonsir29@gmail.com>
2024-09-02 14:43:58 +0200

    add mobile version

--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -13,42 +13,43 @@ const props = defineProps<{
 
 const started = computed(() => props.quizzes.filter(quiz => quiz.state == 'published'))
 const scheduled = computed(() => props.quizzes.filter(quiz => quiz.state == 'locked'))
+const history = computed(() => props.submissions.filter(submission => submission.closed))
 </script>
 
 <template>
-  <div class="w-4/5">
-    <Divider>
+  <div class="w-full p-2 md:w-4/5">
+    <Divider v-if="started.length > 0">
       <h1 class="font-bold text-xl text-primary text-center p-4 whitespace-nowrap">Trwające konkursy</h1>
     </Divider>
     <div v-for="quiz in started" :key="quiz.id" class="rounded-lg bg-white shadow border px-4 py-2 flex items-center justify-between mb-2">
       <div>
-        <p class="font-semibold text-primary">{{ quiz.name }}</p>
+        <p class="font-semibold text-sm 2xs:text-base text-primary">{{ quiz.name }}</p>
         <p class="text-xs py-1">{{ dayjs(quiz.scheduledAt).fromNow() }}</p>
       </div>
-      <FormButton class="min-w-24" small method="post" :href="`/quizzes/${quiz.id}/start`">Weź udział</FormButton>
+      <FormButton class="min-w-24 text-center" small method="post" :href="`/quizzes/${quiz.id}/start`">Weź udział</FormButton>
     </div>
 
-    <Divider>
+    <Divider v-if="scheduled.length > 0">
       <h1 class="font-bold text-xl text-primary text-center p-4 whitespace-nowrap">Nadchodzące konkursy</h1>
     </Divider>
     <div v-for="quiz in scheduled" :key="quiz.id" class="rounded-lg bg-white shadow border px-4 py-2 flex items-center justify-between mb-2">
       <div>
-        <p class="font-semibold text-primary">{{ quiz.name }}</p>
+        <p class="font-semibold text-sm 2xs:text-base text-primary">{{ quiz.name }}</p>
         <p class="text-xs py-1">{{ dayjs(quiz.scheduledAt).fromNow() }}</p>
       </div>
-      <FormButton v-if="!quiz.isUserAssigned" class="min-w-24" small method="post" :href="`/quizzes/${quiz.id}/assign`">Zapisz się</FormButton>
-      <div v-else class="min-w-24 text-center border border-gray-500 text-gray-500 font-semibold rounded-md text-sm py-2 px-3 cursor-not-allowed">Zapisano</div>
+      <FormButton v-if="!quiz.isUserAssigned" class="min-w-24 text-center" small method="post" :href="`/quizzes/${quiz.id}/assign`">Zapisz się</FormButton>
+      <FormButton v-else class="min-w-24 text-center" disabled small method="post" :href="`/quizzes/${quiz.id}/assign`">Zapisano</FormButton>
     </div>
 
-    <Divider>
+    <Divider v-if="history.length > 0">
       <h1 class="font-bold text-xl text-primary text-center p-4 whitespace-nowrap">Historia</h1>
     </Divider>
-    <div v-for="submission in submissions" :key="submission.id" class="rounded-lg bg-white shadow border px-4 py-2 flex items-center justify-between mb-2">
+    <div v-for="submission in history" :key="submission.id" class="rounded-lg bg-white shadow border px-4 py-2 flex items-center justify-between mb-2">
       <div>
-        <p class="font-semibold text-primary">{{ submission.name }}</p>
+        <p class="font-semibold text-sm 2xs:text-base text-primary">{{ submission.name }}</p>
         <p class="text-xs py-1">{{ dayjs(submission.closedAt).fromNow() }}</p>
       </div>
-      <FormButton class="min-w-24" small method="post" :href="`/submissions/${submission.id}/results`">Wyniki</FormButton>
+      <FormButton class="min-w-24 text-center" small method="post" :href="`/submissions/${submission.id}/results`">Wyniki</FormButton>
     </div>
   </div>
 </template>
--- resources/js/components/Common/FormButton.vue
+++ resources/js/components/Common/FormButton.vue
@@ -24,7 +24,7 @@ function handleSubmit() {
       v-if="!disabled"
       type="submit"
       class="font-semibold transition-colors duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-      :class="`${small ? 'rounded-md text-sm py-2' : 'rounded-lg py-3'} ${text ? 'text-black hover:text-primary-800 px-0 py-0' : 'px-3 bg-primary text-white hover:bg-primary-600 shadow-sm'} ${props.class}`"
+      :class="`${small ? 'rounded-md text-xs 2xs:text-sm py-2' : 'rounded-lg py-3'} ${text ? 'text-black hover:text-primary-800 px-0 py-0' : 'px-2 2xs:px-3 bg-primary text-white hover:bg-primary-600 shadow-sm'} ${props.class}`"
     >
       <slot />
     </button>
@@ -33,8 +33,8 @@ function handleSubmit() {
       v-else
       disabled
       type="submit"
-      class="cursor-not-allowed font-semibold bg-primary-100 border px-3"
-      :class="`${small ? 'rounded-md text-sm py-2' : 'rounded-lg py-3'} ${props.class}`"
+      class="border border-gray-500 text-gray-500 font-semibold px-2 2xs:px-3 cursor-not-allowed"
+      :class="`${small ? 'rounded-md text-xs 2xs:text-sm py-2' : 'rounded-lg py-3'} ${props.class}`"
     >
       <slot />
     </button>
