a02e6fa1e9419df8b16c6a27994d978768974b0d
AmonDeShir <amonsir29@gmail.com>
2024-09-03 20:26:58 +0200

    fix code style

--- app/Http/Resources/QuizSubmissionResource.php
+++ app/Http/Resources/QuizSubmissionResource.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\Answer;
-use App\Models\AnswerRecord;
 use App\Models\Question;
 use App\Models\Quiz;
 use Carbon\Carbon;
@@ -26,7 +25,7 @@ class DatabaseSeeder extends Seeder
 
         foreach ($questions as $question) {
             $answers = Answer::factory()->count(4)->create(["question_id" => $question->id]);
-            $question->correct_answer_id = ($answers[0]->id);
+            $question->correct_answer_id = $answers[0]->id;
             $question->save();
         }
     }
