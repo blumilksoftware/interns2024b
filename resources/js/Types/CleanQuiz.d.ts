e483a77ec971b64ad2d34f63b6d57cab321c263a
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-09-03 11:12:58 +0200

    fix: add id to cleanQuiz

--- resources/js/Types/CleanQuiz.d.ts
+++ resources/js/Types/CleanQuiz.d.ts
@@ -1,6 +1,7 @@
 import {type CleanQuestion} from '@/Types/CleanQuestion'
 
 export interface CleanQuiz {
+  id: number
   name: string
   scheduledAt?: number
   duration?: number
--- resources/js/Types/Quiz.d.ts
+++ resources/js/Types/Quiz.d.ts
@@ -1,7 +1,6 @@
 import {type CleanQuiz} from '@/Types/CleanQuiz'
 
 export interface Quiz extends CleanQuiz {
-  id: number
   createdAt: number
   updatedAt: number
   state: 'unlocked' | 'locked' | 'published'
