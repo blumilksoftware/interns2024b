ac2b7ea7855f31a527347190802b5b983637b621
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-09-04 11:04:01 +0200

    Remove not existing policy from AuthServiceProvider

--- app/Policies/AuthServiceProvider.php
+++ app/Policies/AuthServiceProvider.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace App\Policies;
 
 use App\Http\Requests\UserRequest;
-use App\Models\Quiz;
 
 class AuthServiceProvider
 {
     protected $policies = [
         UserRequest::class => UserPolicy::class,
-        Quiz::class => RankingPolicy::class,
     ];
 }
