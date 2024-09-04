493cc1790e716aba2d67594dfae474e1e7470c91
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-09-04 14:10:28 +0200

    Fix code style

--- resources/js/Pages/Guest/ResetPassword.vue
+++ resources/js/Pages/Guest/ResetPassword.vue
@@ -2,8 +2,8 @@
 import { useForm } from '@inertiajs/vue3'
 import CustomInput from '@/components/Common/CustomInput.vue'
 import { Head } from '@inertiajs/vue3'
-import {PageProps} from "@/Types/PageProps";
-import DisplayError from "@/components/Common/DisplayError.vue";
+import type { PageProps } from '@/Types/PageProps'
+import DisplayError from '@/components/Common/DisplayError.vue'
 
 const props = defineProps<{
   token: string
@@ -30,12 +30,12 @@ function submit() {
 
   <div class="sm:p-6 sm:pt-12 size-full flex justify-center sm:h-fit">
     <form class="p-6 gap-4 flex flex-col size-full bg-white shadow border sm:rounded-lg sm:max-w-lg sm:h-fit" @submit.prevent="submit">
-        <CustomInput v-model="form.password" label="Nowe hasło" :error="errors?.password" name="password" type="password" />
-        <CustomInput v-model="form.password_confirmation" label="Powtórz hasło" :error="errors?.password_confirmation" name="password_confirmation" type="password" />
-        <DisplayError :error="errors?.email" />
-        <button :disabled="form.processing" type="submit" class="bg-primary text-white font-bold py-3 px-4 rounded-lg disabled:bg-primary/70">
-          Zmień hasło
-        </button>
-      </form>
+      <CustomInput v-model="form.password" label="Nowe hasło" :error="errors?.password" name="password" type="password" />
+      <CustomInput v-model="form.password_confirmation" label="Powtórz hasło" :error="errors?.password_confirmation" name="password_confirmation" type="password" />
+      <DisplayError :error="errors?.email" />
+      <button :disabled="form.processing" type="submit" class="bg-primary text-white font-bold py-3 px-4 rounded-lg disabled:bg-primary/70">
+        Zmień hasło
+      </button>
+    </form>
   </div>
 </template>
--- resources/js/components/Common/DisplayError.vue
+++ resources/js/components/Common/DisplayError.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-  defineProps<{
-    error?: string,
+defineProps<{
+    error?: string
   }>()
 </script>
 
 <template>
   <div v-if="error" class="font-semibold text-sm text-red text-left">
-    {{error}}
+    {{ error }}
   </div>
 </template>
