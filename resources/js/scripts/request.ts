de25ef14ba6b00fe38496f9fb8069f0b7d25c87c
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-09-04 22:48:22 +0200

    chore: add visit payload type

--- resources/js/scripts/request.ts
+++ resources/js/scripts/request.ts
@@ -1,18 +1,18 @@
+import { type VisitPayload } from '@/Types/VisitPayload'
 import { router } from '@inertiajs/vue3'
 import { ref } from 'vue'
 
 export class Request{
   public isRequestOngoing = ref<boolean>(false)
   public error = ref<string>('')
-  sendRequest(url : string, method: 'POST' | 'PATCH' | 'DELETE', payload? : Record<string,any>) {
+  sendRequest(url : string, payload : VisitPayload) {
     payload = {
       ...payload,
       preserveScroll: true,
       preserveState: true,
       onStart: () => this.isRequestOngoing.value = true,
       onFinish: () =>  this.isRequestOngoing.value = false,
-      onError: (error:{ name: string }) => this.error.value = error.name,
-      method: method,
+      onError: ({name}) => this.error.value = name,
     }
     router.visit(url, payload)
   }
