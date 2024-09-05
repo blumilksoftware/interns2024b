fc2c53ce0c2d0199049c937d25cc46cdeba43328
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-09-05 15:03:57 +0200

    chore: changed seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\Answer;
-use App\Models\AnswerRecord;
-use App\Models\Question;
 use App\Models\Quiz;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
@@ -21,28 +18,9 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             UserQuizSeeder::class,
         ]);
-        // $quiz = Quiz::factory()->create();
-        // $questions = Question::factory()->count(10)->create(["quiz_id" => $quiz->id]);
-        // foreach ($questions as $question) {
-        //     Answer::factory()->count(4)->create(["question_id" => $question->id]);
-        // }
-        // AnswerRecord::factory()->create();
-
         Quiz::factory()->create([
-            "name" => "unlocked"
-        ]);
-
-        Quiz::factory()->create([
-            "name" => "locked",
-            "locked_at" => Carbon::now()->subMinutes(10),
+            "name" => "quiz_name",
             "scheduled_at" => Carbon::now()->addMinutes(60),
-            "duration" => 120,
-        ]);
-
-        Quiz::factory()->create([
-            "name" => "published",
-            "locked_at" => Carbon::now()->subMinutes(60),
-            "scheduled_at" => Carbon::now()->subMinutes(10),
             "duration" => 120,
         ]);
     }
