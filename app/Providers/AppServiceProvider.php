7fdcbbb7545f50383474f5d8c9aa972db0828b68
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-09-05 13:35:05 +0200

    Add custom mail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,5 +18,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->view('emails.auth.verify', [
+                    'user' => $notifiable,
+                    'url' => $url]);
+        });
     }
 }
---
+++ resources/views/emails/auth/verify.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to [Company Name]</title>
+</head>
+
+<body>
+<header>
+    <img src="" alt="[Company Name] Logo" width="150" height="50">
+    <h1>[Company Name]</h1>
+</header>
+
+<body>
+<p>Witaj,</p>
+<a href="{{ $url }}">Link </a>
+</body>
+<footer>
+</footer>
+</body>
+
+</html>
