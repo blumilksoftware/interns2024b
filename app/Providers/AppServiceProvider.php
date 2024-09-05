75cd673ae8c31bd15d9e45e073e50e2fb6506672
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-09-05 13:35:30 +0200

    fix code style

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,10 +19,10 @@ class AppServiceProvider extends ServiceProvider
     {
         JsonResource::withoutWrapping();
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-            return (new MailMessage)
-                ->view('emails.auth.verify', [
-                    'user' => $notifiable,
-                    'url' => $url]);
+            return (new MailMessage())
+                ->view("emails.auth.verify", [
+                    "user" => $notifiable,
+                    "url" => $url]);
         });
     }
 }
