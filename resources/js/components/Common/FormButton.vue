80b37b1d6932edd78a1d38fd09fb28fa73d93e59
AmonDeShir <amonsir29@gmail.com>
2024-09-06 10:38:38 +0200

    Migrate button from useForm to router

--- resources/js/components/Common/FormButton.vue
+++ resources/js/components/Common/FormButton.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 
-import { useForm } from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3'
+import {ref} from 'vue'
 
-const form = useForm({})
 const props = withDefaults(defineProps<{
   small?: boolean
   disabled?: boolean
@@ -10,18 +10,32 @@ const props = withDefaults(defineProps<{
   class?: string
   href: string
   method?: 'post' | 'get' | 'put' | 'patch' | 'delete'
-  options?: any
-}>(), { method: 'get', class: '', options: {}})
+  data?: any
+  preserveState?: boolean
+  preserveScroll?: boolean
+  only?: string[]
+}>(), { method: 'get', class: '', options: undefined, data: undefined, only: undefined })
+
+const processing = ref(false)
+const emit = defineEmits(['click', 'finish'])
 
 function handleSubmit() {
-  form[props.method](props.href, props.options)
+  processing.value = true
+  emit('click')
+
+  router[props.method](props.href, props.data, {
+    onSuccess: () => { processing.value = false; emit('finish')},
+    only: props.only,
+    preserveState: props.preserveState,
+    preserveScroll: props.preserveScroll,
+  })
 }
 </script>
 
 <template>
   <form :class="props.class" @submit.prevent="handleSubmit">
     <button
-      v-if="!disabled && !form.processing"
+      v-if="!disabled && !processing"
       type="submit"
       class="font-semibold transition-colors duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
       :class="`${small ? 'rounded-md text-xs 2xs:text-sm py-2' : 'rounded-lg py-3'} ${text ? 'text-black hover:text-primary-800 px-0 py-0' : 'px-2 2xs:px-3 bg-primary text-white hover:bg-primary-600 shadow-sm'} ${props.class}`"
