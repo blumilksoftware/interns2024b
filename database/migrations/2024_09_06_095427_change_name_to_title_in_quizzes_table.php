1cf0ae408a229837a331818d8987136ac5432e3b
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-09-08 19:10:53 +0200

    make fix

--- database/migrations/2024_09_06_095427_change_name_to_title_in_quizzes_table.php
+++ database/migrations/2024_09_06_095427_change_name_to_title_in_quizzes_table.php
@@ -1,27 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class() extends Migration {
     public function up(): void
     {
-        Schema::table('quizzes', function (Blueprint $table) {
+        Schema::table("quizzes", function (Blueprint $table): void {
             $table->renameColumn("name", "title");
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('quizzes', function (Blueprint $table) {
+        Schema::table("quizzes", function (Blueprint $table): void {
             $table->renameColumn("title", "name");
         });
     }
