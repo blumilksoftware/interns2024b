25ee251ecae4e628c2d38b73e6d267b1fc9cfcc9
AmonDeShir <amonsir29@gmail.com>
2024-09-10 12:04:36 +0200

    fix linter error

--- resources/js/Helpers/Timer.ts
+++ resources/js/Helpers/Timer.ts
@@ -1,14 +1,14 @@
 import {computed, ref} from 'vue'
 import {calcSecondsLeftToDate, secondsToHour, timeToString} from '@/Helpers/Time'
 
-export function useTimer(to?: string | number, timeout?: () => void) {
+export function useTimer(to: string | number, timeout: () => void) {
   const left = ref(calcSecondsLeftToDate(to))
 
   const interval = setInterval(() => {
     left.value = Math.max(0, left.value - 1)
 
     if (left.value === 0) {
-      timeout!()
+      timeout();
       clearInterval(interval)
     }
   }, 1000)
