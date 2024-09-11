01a9b7a4a13284d8f0ff4cfd938de269f19ca530
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-09-11 20:24:39 +0200

    Add crud user

--- resources/js/Pages/Admin/UsersPanel.vue
+++ resources/js/Pages/Admin/UsersPanel.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import {type User} from '@/Types/User'
+import FormButton from '@/components/Common/FormButton.vue'
+import LinkButton from '@/components/Common/LinkButton.vue'
+import {type PageProps} from '@/Types/PageProps'
+
+defineProps<{
+  users: User[]
+}&PageProps>()
+</script>
+
 <template>
-  Users Panel
+  <div class="flex flex-col items-start gap-4">
+    <div v-for="listedUser in users" :key="listedUser.id" class="bg-white border shadow rounded-md p-4 w-full max-w-xl">
+      <div>ID: {{ listedUser.id }}</div>
+      <div>Imię: {{ listedUser.name }}</div>
+      <div>Nazwisko: {{ listedUser.surname }}</div>
+      <div>E-mail: {{ listedUser.email }}</div>
+      <div>Szkoła: {{ listedUser.school.name }}</div>
+      <div class="flex gap-4">
+        <FormButton :disabled="listedUser.isAnonymized || !user?.isSuperAdmin" :href="`/admin/users/${listedUser.id}/anonymize`" method="patch" class="w-full" button-class="w-full">Anonimizuj użytkownika</FormButton>
+        <LinkButton :href="`/admin/users/${listedUser.id}/edit`">Edit</LinkButton>
+      </div>
+    </div>
+  </div>
 </template>
