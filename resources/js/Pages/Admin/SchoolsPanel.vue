e41f1f6b19ea72aa4d634e044f721ea0c58062d7
Amon De Shir <amonsir29@gmail.com>
2024-09-11 22:44:59 +0200

    update crud button style

--- resources/js/Pages/Admin/SchoolsPanel.vue
+++ resources/js/Pages/Admin/SchoolsPanel.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import {Head} from '@inertiajs/vue3'
+import LinkButton from "@/components/Common/LinkButton.vue";
+import FormButton from "@/components/Common/FormButton.vue";
+import {School} from "@/Types/School";
+
+defineProps<{ schools: School[] }>()
+
 </script>
 
 <template>
-  <Head>
-    <title>Szkoły</title>
-  </Head>
+  <div class="flex flex-col items-start gap-4">
+    <div v-for="school in schools" :key="school.id" class="bg-white border shadow rounded-md p-4 w-full max-w-xl">
+      <div>ID: {{ school.id }}</div>
+      <div>Nazwa: {{ school.name }}</div>
+      <div>Miasto: {{ school.city }}</div>
 
-  Quizzes - CRUD
+      <div class="flex gap-2 p-2">
+        <LinkButton small :href="`/admin/schools/${school.id}/edit`">Edit</LinkButton>
+        <FormButton small :href="`/admin/schools/${school.id}`" method="delete">Delete</FormButton>
+      </div>
+    </div>
+
+    <FormButton href="/admin/schools/create" method="get" class="w-full" button-class="w-full">Dodaj nową szkołę</FormButton>
+
+  </div>
 </template>
