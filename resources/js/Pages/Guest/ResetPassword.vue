a227a0916b0ea96b1b972573b90a2081d11ae867
Mykyta Mykulskyi <81817136+JokeUrSelf@users.noreply.github.com>
2024-10-04 12:05:42 +0200

    Update resources/js/Pages/Guest/ResetPassword.vue
    Co-authored-by: Ewelina Skrzypacz <56546832+EwelinaSkrzypacz@users.noreply.github.com>

--- resources/js/Pages/Guest/ResetPassword.vue
+++ resources/js/Pages/Guest/ResetPassword.vue
@@ -37,7 +37,7 @@ function submit() {
   </Head>
   <div class="sm:p-6 sm:pt-12 size-full flex flex-col items-center justify-center sm:h-fit gap-6">
     <div class="text-md font-medium leading-6 text-gray-900 px-[5vw] text-center flex flex-col gap-1">
-      <p>Wprowadź nowe hasło z którego będziesz korzystać w aplikacji.</p>
+      <p>Wprowadź nowe hasło, z którego będziesz korzystać w aplikacji.</p>
     </div>
     <form class="p-6 gap-4 flex flex-col size-full sm:rounded-lg sm:max-w-lg sm:h-fit" @submit.prevent="submit">
       <CustomInput v-model="form.password" label="Nowe hasło" :error="errors?.password" name="password" :type="isVisiblePassword ? 'text' : 'password'">
