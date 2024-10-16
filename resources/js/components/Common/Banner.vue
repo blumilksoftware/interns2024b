35fbb07016659a64d28a73da533ce32cb30db22b
Amon De Shir <amonsir29@gmail.com>
2024-10-16 15:22:50 +0200

    make banner sticky

--- resources/js/components/Common/Banner.vue
+++ resources/js/components/Common/Banner.vue
@@ -2,17 +2,17 @@
 import { XMarkIcon } from '@heroicons/vue/20/solid'
 
 defineProps<{text:string}>()
-defineEmits<{ click: [] }>()
+const emit = defineEmits<{ click: [] }>()
 </script>
 
 <template>
-  <div class="flex absolute top-0 w-full items-center gap-x-6 bg-primary px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+  <div class="text-white z-50 flex sticky top-0 w-full items-center gap-x-6 bg-primary px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
     <b class="text-white">
       {{ text }}
     </b>
     <div class="flex flex-1 justify-end">
       <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
-        <XMarkIcon class="size-5 text-white" aria-hidden="true" @click="$emit('click')" />
+        <XMarkIcon class="size-5 text-white" aria-hidden="true" @click="emit('click')" />
       </button>
     </div>
   </div>
