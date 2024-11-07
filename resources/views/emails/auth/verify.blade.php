d635a839acf5e01e17b5e36b2b06baf601dc22de
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-11-07 13:22:07 +0100

    style: apply suggestions from code review

--- resources/views/emails/auth/verify.blade.php
+++ resources/views/emails/auth/verify.blade.php
@@ -3,39 +3,34 @@
 <style>
     * { margin: 0; padding: 0; }
     a { text-decoration: none; }
-    p { padding: 14px 0; }
-    table { width: 100%; border-spacing: 0; margin-top: 8px; }
-    .button { background-color: #262C89; color: white; padding: 12px 20px; border-radius: 16px; }
-    .footer { color: #6b7280; margin-top: 16px; }
+    p { margin: 48px 0; line-height: 24px; }
+    table { width: 100%; border-spacing: 0; }
+    .button { background-color: #262C89; color: white; padding: 16px 80px; border-radius: 12px; }
 </style>
 
-<div style="font-family: sans-serif; color: #3D4852;">
-    <table><tr><td style="vertical-align: middle; width: 50px;">
-        <img style="margin: 5px;" src="{{ asset('logo.png') }}" alt="interns2024b Logo" height="40" width="40"></td><td>
-        <h2>interns2024b</h2>
-    </td></tr></table>
+<div style="font-family: sans-serif; color: #3D4852; padding: 0 48px;">
+    <div style="margin: 40px 0 80px 0;">
+        <img src="{{ asset('logo.png') }}" alt="interns2024b Logo" height="50" width="50">
+    </div>
 
-    <div style="padding: 16px 48px;">
-        <h3>Cześć {{ $user->name }},</h3>
+    <div>
+        <h2>Cześć, {{ $user->name }}!</h2>
 
-        <p>Aby zweryfikować swoje konto, kliknij poniższy przycisk:</p>
+        <p>Otrzymaliśmy prośbę o wysłanie linku aktywacyjnego do Twojego konta. Kliknij poniższy przycisk, aby zweryfikować swój adres e-mail.</p>
         
         <table style="margin: 24px auto;"><tr><td style="text-align: center;">
             <b><a class="button" href="{{ $url }}">Zweryfikuj e-mail</a></b>
         </td></tr></table>
 
-        <span style="color: #B32F2F; font-size: .9rem;">
-            Jeśli to nie Ty wysłałeś/aś tę prośbę, po prostu zignoruj tę wiadomość.
-        </span>
+        <p>Jeśli prośba nie została zainicjonowana przez Ciebie, możesz zignorować tę wiadomość.</p>
 
-        <p class="footer">{{ config('app.name') }}</p>
+        <p style="margin-top: 16px; ">Pozdrawiamy,<br><span style="color: #262C89;">{{ config('app.name') }}</span></p>
 
-        <hr style="margin: 16px 0; color: #6b7280">
-
-        <p>
-            <span style="color: #6b7280;">Alternatywnie, użyj link:</span>
-            <a href="{{ $url }}">{{ $url }}</a>
-        </p>
+        <hr style="border: 1px solid #e8ebf1">
+        
+        <table><tr><td style="padding-top: 32px; text-align: center;">
+            © 2024 {{ config('app.name') }}. Wszelkie prawa zastrzeżone.
+        </td></tr></table>
     </div>
 </div>
 </html>
