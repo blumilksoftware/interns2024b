46d26e043f231e42c649b2821d5464691c027b41
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-11-08 23:20:11 +0100

    fix: duration input doesn't resize fully

--- resources/js/components/QuizzesPanel/InputWrapper.vue
+++ resources/js/components/QuizzesPanel/InputWrapper.vue
@@ -3,8 +3,8 @@ defineProps<{ label?:string, hasContent:boolean, error?:string, showError:boolea
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <label v-if="label" class="flex flex-col 2xs:flex-row 2xs:gap-1">
+  <div class="flex flex-1 flex-col">
+    <label v-if="label" class="flex flex-1 flex-col 2xs:flex-row 2xs:gap-1">
       {{ label }}
       <slot v-if="hasContent" />
       <b v-else aria-label="brak danych">-</b>
