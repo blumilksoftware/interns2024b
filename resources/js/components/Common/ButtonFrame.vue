abc8723b4730e6f47bd7d5126d4f1fe527bf03ea
Amon De Shir <amonsir29@gmail.com>
2024-11-11 20:08:16 +0100

    add padding to ButtonFrame

--- resources/js/components/Common/ButtonFrame.vue
+++ resources/js/components/Common/ButtonFrame.vue
@@ -2,6 +2,7 @@
 import { type ButtonFrameProps } from '@/Types/ButtonFrameProps'
 
 const slots = defineSlots()
+const defaultSlot = slots.default()[0]
 defineProps<ButtonFrameProps>()
 </script>
 
@@ -15,7 +16,8 @@ defineProps<ButtonFrameProps>()
       'focus:rounded-sm text-black hover:text-primary-800 p-0 font-semibold': text,
       'px-4 bg-primary text-white hover:bg-primary-950 py-2.5 font-bold': !text,
       'opacity-50 pointer-events-none': disabled,
-      'pl-3': slots.default,
+      'pl-3': defaultSlot?.children?.at(0)?.type !== Symbol.for('v-txt'),
+      'pr-3': defaultSlot?.children?.at(-1)?.type !== Symbol.for('v-txt'),
     }"
   >
     <slot />
