6fcc5b46afc15848b1280b563ea030fa9f753a2d
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-11-18 14:00:54 +0100

    fix: remove dot

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,6 +2,6 @@
 
 @php
   $code = '500';
-  $message = 'Wystąpił błąd serwera.';
+  $message = 'Wystąpił błąd serwera';
   $description = 'Wystąpił nieoczekiwany problem po stronie serwera, który uniemożliwił przetworzenie Twojego żądania. Prosimy spróbować ponownie później.';
 @endphp
