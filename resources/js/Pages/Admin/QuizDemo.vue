06d1404709e3f94e5a972990e5b43899b1c6a1fd
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-11-23 00:46:50 +0100

    Revert "feat: add demo caution banner"
    This reverts commit 0636a76834cb8ac8cd592b24fef56f817fae815c.

--- resources/js/Pages/Admin/QuizDemo.vue
+++ resources/js/Pages/Admin/QuizDemo.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import CautionBanner from '@/components/Common/CautionBanner.vue'
 import QuizLayout from '@/Layouts/QuizLayout.vue'
 import { converter } from '@/Helpers/Converter'
 
@@ -7,7 +6,6 @@ const props = defineProps<{ quiz: Quiz }>()
 </script>
 
 <template>
-  <CautionBanner class="mb-5" text="To jest wersja demonstracyjna strony testu" />
   <QuizLayout
     :user-quiz="converter.quiz.adminToUser(props.quiz)"
     :request-close-quiz="{ method: 'get', href: '/admin/quizzes/' }"
