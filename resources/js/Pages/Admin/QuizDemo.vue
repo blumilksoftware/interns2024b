9eb85a01ada215a6046b9554948e3efa7c8afd11
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-11-23 01:16:14 +0100

    feat: add demo banner

--- resources/js/Pages/Admin/QuizDemo.vue
+++ resources/js/Pages/Admin/QuizDemo.vue
@@ -6,6 +6,7 @@ const props = defineProps<{ quiz: Quiz }>()
 </script>
 
 <template>
+  <div class="w-full bg-primary text-white text-center font-bold mb-4 p-1">Wersja demonstracyjna</div>
   <QuizLayout
     :user-quiz="converter.quiz.adminToUser(props.quiz)"
     :request-close-quiz="{ method: 'get', href: '/admin/quizzes/' }"
