bf981700d0ce51996808bf25b917654795ef4a4d
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-11-24 01:34:29 +0100

    feat: add theme switch

---
+++ resources/js/components/Common/ThemeSwitch.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+function toggleTheme() {
+  const isThemeTauron = document.documentElement.classList.contains('theme-tauron')
+  document.documentElement.classList.remove(isThemeTauron ? 'theme-tauron' : 'theme-witelon')
+  document.documentElement.classList.add(isThemeTauron ? 'theme-witelon' : 'theme-tauron')
+}
+</script>
+
+<template>
+  <label class="inline-flex items-center cursor-pointer">
+    <input type="checkbox" class="sr-only peer" @click="toggleTheme">
+    <div class="
+      relative
+      w-11
+      h-6
+      bg-primary
+      peer-focus:outline-none
+      peer-focus:ring-2
+      rounded-full
+      
+      after:bg-white
+      after:content-['']
+      after:start-0.5
+      after:absolute after:top-0.5
+      after:border
+      after:rounded-full
+      after:size-5
+      peer-checked:after:translate-x-full
+      rtl:peer-checked:after:-translate-x-full
+      after:transition-all
+      "
+    />
+    <span class="ms-3 text-sm font-medium text-gray-500">Zmień skórkę</span>
+  </label>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -49,82 +49,31 @@ module.exports = {
             },
             colors: {
                 'primary': {
-                    'DEFAULT': '#262c89',
-                    '50': '#ecf3ff',
-                    '100': '#dde8ff',
-                    '200': '#c2d5ff',
-                    '300': '#9db9ff',
-                    '400': '#7691ff',
-                    '500': '#556aff',
-                    '600': '#373ff4',
-                    '700': '#2a2fd8',
-                    '800': '#252bae',
-                    '900': '#262c89',
-                    '950': '#141647',
-                },
-                'secondary': {
-                    'DEFAULT': '#c9cefc',
-                    '50': '#eff0fe',
-                    '100': '#e1e5fe',
-                    '200': '#c9cefc',
-                    '300': '#a8aef9',
-                    '400': '#8585f4',
-                    '500': '#7268ec',
-                    '600': '#634cdf',
-                    '700': '#563dc5',
-                    '800': '#46349f',
-                    '900': '#363765',
-                    '950': '#241d49',
-                },
-                'accent': {
-                    'DEFAULT': '#6566C2',
-                    '50': '#f2f5fb',
-                    '100': '#e7ecf8',
-                    '200': '#d3dbf2',
-                    '300': '#b8c3e9',
-                    '400': '#9aa6df',
-                    '500': '#8189d3',
-                    '600': '#6566c2',
-                    '700': '#5757ab',
-                    '800': '#48488b',
-                    '900': '#40426f',
-                    '950': '#252541',
-                },
-                'rose': {
-                    'DEFAULT': '#f70279',
-                    '50': '#fff0f9',
-                    '100': '#ffe3f5',
-                    '200': '#ffc7eb',
-                    '300': '#ff99d9',
-                    '400': '#ff59be',
-                    '500': '#ff29a2',
-                    '600': '#f70279',
-                    '700': '#dd0061',
-                    '800': '#b60050',
-                    '900': '#970445',
-                    '950': '#5e0025',
+                    'DEFAULT': 'rgb(var(--color-primary))',
+                    '800': 'var(--color-primary-bright)',
+                    '950': 'var(--color-primary-dark)',
                 },
                 'red': {
                     'DEFAULT': '#ff0000',
-                    '50': '#fff0f0',
-                    '100': '#ffdddd',
-                    '200': '#ffc0c0',
-                    '300': '#ff9494',
-                    '400': '#ff5757',
                     '500': '#ff2323',
-                    '600': '#ff0000',
-                    '700': '#d70000',
-                    '800': '#b10303',
-                    '900': '#920a0a',
-                    '950': '#500000',
                 },
-                "gradient":{"start":"#9198e5", "end":"#1f0843"},
+                "gradient": {
+                    "start":"#9198e5",
+                    "end":"#1f0843"
+                },
             }
         },
     },
     plugins: [
-        plugin(function ({ addComponents, addVariant }) {
+        plugin(function ({ addComponents, addVariant, addBase }) {
             addVariant('hover-focus', ['&:hover', '&:focus']);
+            addBase({
+              ':root': {
+                '--color-primary': '38 44 137',
+                '--color-primary-bright': '37 43 174',
+                '--color-primary-dark': '20 22 71',
+              },
+            });
             addComponents({
                 '.icon': {
                   '@apply size-6 stroke-2 text-primary hover:text-primary-800 transition-colors duration-200':{},
