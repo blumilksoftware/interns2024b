ae10862d577bd379a19027aecd222984df905e00
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-12-01 23:08:25 +0100

    Revert "fix: theme state is preseved between page hoops"
    This reverts commit 059e5eb04646cd6d9381f5b4b6f495dccced4046.

--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -3,18 +3,11 @@ import Header from '@/components/Common/Header.vue'
 import Footer from '@/components/Common/Footer.vue'
 import BackgroundEffect from '@/components/Common/BackgroundEffect.vue'
 import Banner from '@/components/Common/Banner.vue'
-import {onMounted, ref, watch} from 'vue'
+import {ref, watch} from 'vue'
 import {type PageProps} from '@/Types/PageProps'
 
 const props = defineProps<{ pages: Page[] } & PageProps>()
 const status = ref<string | undefined>(props.flash.status)
-
-onMounted(()=>{
-  const themeClass = props.user?.theme
-  if (themeClass) {
-    document.documentElement.classList.add(themeClass)
-  }
-})
 
 watch(() => props.flash, flash => {
   status.value = flash.status
@@ -27,7 +20,7 @@ watch(() => props.flash, flash => {
 
   <div class="flex flex-col items-center h-full min-h-screen">
     <Banner v-model="status" />
-    <Header :pages="props.pages" :user="props.user" :app-name="props.appName" />
+    <Header :pages :user="props.user" :app-name="props.appName" />
     <slot />
     <Footer />
   </div>
