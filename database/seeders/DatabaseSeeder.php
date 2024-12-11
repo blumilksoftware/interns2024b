3a18bfa12b3e1844bebf532987647031ac088f43
Amon De Shir <amonsir29@gmail.com>
2024-12-11 06:36:36 +0100

    add archived quiz to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,9 @@ namespace Database\Seeders;
 use App\Models\Answer;
 use App\Models\Question;
 use App\Models\Quiz;
+use App\Models\School;
+use App\Models\User;
+use App\Models\UserQuiz;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +23,10 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Quiz::factory()
+            ->has(Question::factory()->has(Answer::factory()->count(4)))
+            ->create(["locked_at" => Carbon::now()->subHour(), "duration" => 60, "scheduled_at" => Carbon::now()]);
+
+        Quiz::factory()
             ->has(Question::factory()->count(5)->has(Answer::factory()->count(4)))
             ->count(3)
             ->create(["locked_at" => null, "duration" => 1, "scheduled_at" => Carbon::now()->addHour()]);
@@ -29,8 +36,20 @@ class DatabaseSeeder extends Seeder
             ->count(3)
             ->create(["locked_at" => null, "duration" => 1, "scheduled_at" => Carbon::now()->addMinutes(2)]);
 
+        $archivedQuiz = Quiz::factory()
+            ->has(Question::factory()->has(Answer::factory()->count(4)))
+            ->create(["locked_at" => Carbon::now()->subDays(2), "duration" => 60, "scheduled_at" => Carbon::now()->subDay()]);
+
         foreach (Quiz::all() as $quiz) {
             PublishedQuizSeeder::selectRandomCorrectAnswer($quiz);
+        }
+
+        User::factory()->count(10)->has(School::factory())->create();
+        User::factory()->count(10)->has(School::factory())->create();
+        User::factory()->count(10)->has(School::factory())->create();
+
+        foreach (User::query()->role("user")->get() as $user) {
+            UserQuizSeeder::createUserQuizForUser($archivedQuiz, $user, null);
         }
     }
 }
