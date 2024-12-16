955fb1d55a3ecd3d6ac415910538f60f20cf1eb0
Dominik Purgał <amonsir29@gmail.com>
2024-12-16 01:21:19 +0100

    replace switch with select

--- resources/js/components/QuizzesPanel/QuizHeader.vue
+++ resources/js/components/QuizzesPanel/QuizHeader.vue
@@ -4,6 +4,7 @@ import InputWrapper from '@/components/QuizzesPanel/InputWrapper.vue'
 import CustomDatepicker from '@/components/Common/CustomDatepicker.vue'
 import { formatDate } from '@/Helpers/Format'
 import CrudInput from '@/components/Crud/CrudInput.vue'
+import CrudSelect from '@/components/Crud/CrudSelect.vue'
 
 defineProps<{
   editing: boolean
@@ -54,44 +55,27 @@ const quiz = defineModel<Quiz>({ required: true })
       name="duration"
       label="Czas trwania testu (min):"
       :error="errors.duration"
+      class="duration-200"
+      :class="{ 'text-sm text-gray-600' : !selected }"
       :editing="editing"
-      class="min-h-6.5 duration-200"
-      :class="{ 'text-sm text-gray-600' : !editing }"
     />
+
 
     <div
       class="flex items-center duration-200 min-h-6.5"
       :class="{ 'text-sm text-gray-600' : !selected }"
     >
-      <p>Widoczność: </p>
-      <label
-        class="inline-flex items-center cursor-pointer border-b border-transparent bg-transparent focus:border-b-primary"
-        :class="{'pl-2 border-b-primary/30 hover:border-b-primary/60 text-primary text-center duration-200 transition-colors': editing}"
-      >
-        <input v-if="editing" type="checkbox" class="sr-only peer" :checked="quiz.isPublic" @click="(event: any) => quiz.isPublic = event.currentTarget.checked">
-        <div v-if="editing" class="
-          relative
-          w-7
-          h-4
-          bg-primary
-          peer-focus:outline-none
-          peer-focus:ring-2
-          rounded-full
-
-          after:bg-white
-          after:content-['']
-          after:start-0.5
-          after:absolute after:top-0.5
-          after:border
-          after:rounded-full
-          after:size-3
-          peer-checked:after:translate-x-full
-          rtl:peer-checked:after:-translate-x-full
-          after:transition-all
-          "
-        />
-        <b class="select-none" :class="{'pl-2 text-primary': editing, 'pl-1': !editing}">{{ quiz.isPublic ? 'Dostępny dla wszystkich' : 'Dostępny tylko dla zaproszonych' }}</b>
-      </label>
+      <CrudSelect
+        label="Widoczność: "
+        :editing="editing"
+        :value="quiz.isPublic?.toString() ?? 'false'"
+        :error="errors.is_public"
+        :items="[
+          { key: 'false', text: 'Dostępny tylko dla zaproszonych' },
+          { key: 'true', text: 'Dostępny dla wszystkich' }
+        ]"
+        @change="quiz.isPublic = $event === 'true'"
+      />
     </div>
   </div>
 </template>
