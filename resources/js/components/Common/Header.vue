575cf40ddf17720b2525890c27f13299d8f7e9d1
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-12-18 23:14:11 +0100

    style: redesign sidebar

--- resources/js/components/Common/Header.vue
+++ resources/js/components/Common/Header.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import FormButton from '@/components/Common/FormButton.vue'
 import { TransitionRoot, Dialog, TransitionChild, DialogPanel, DialogTitle } from '@headlessui/vue'
-import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Bars3Icon, FlagIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { ref } from 'vue'
 import LogoIcon from '@/components/Icons/LogoIcon.vue'
+import Expand from './Expand.vue'
 
 defineProps<{ pages: Page[], user?: User, appName: string, title?: string }>()
 const open = ref<boolean>(false)
@@ -68,78 +69,54 @@ const isSelected = (page: Page) => page.href === window.location.pathname
       </div>
     </nav>
 
-    <TransitionRoot
-      as="template"
-      :show="open"
+    <div
+      class="bg-white h-full top-0 absolute left-full duration-200 flex flex-col gap-4 p-4 sm:hidden min-w-[50%] scale-95"
+      :class="{'-translate-x-full shadow-lg !scale-100':open}"
     >
-      <Dialog
-        class="relative z-10"
-        @close="open = false"
+      <div class="flex justify-between option !py-3">
+        <span class="font-bold">
+          {{ appName }}
+        </span>
+
+        <button @click="open=false">
+          <XMarkIcon class="size-5 stroke-2" />
+        </button>
+      </div>
+
+      <span class="text-sm text-gray-400 px-3">
+        Opcje
+      </span>
+
+      <a
+        v-for="page in pages"
+        :key="page.title"
+        :href="page.href"
+        class="option"
+        :class="{'!text-primary': isSelected(page) }"
       >
-        <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-          <TransitionChild
-            as="template"
-            enter="transform transition ease-in-out duration-500 sm:duration-700"
-            enter-from="translate-x-full"
-            enter-to="translate-x-0"
-            leave="transform transition ease-in-out duration-500 sm:duration-700"
-            leave-from="translate-x-0"
-            leave-to="translate-x-full"
-          >
-            <DialogPanel class="pointer-events-auto w-screen max-w-80">
-              <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                <div class="px-4 sm:px-6 flex items-start justify-between">
-                  <DialogTitle class="text-base font-semibold leading-6 text-gray-900">
-                    {{ appName }}
-                  </DialogTitle>
+        {{ page.title }}
+      </a>
 
-                  <div class="ml-3 flex h-7 items-center">
-                    <button
-                      type="button"
-                      class="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      @click="open = false"
-                    >
-                      <span class="sr-only">
-                        Zamknij Menu
-                      </span>
+      <span class="text-sm text-gray-400 px-3 mt-auto">
+        Sesja
+      </span>
 
-                      <XMarkIcon
-                        class="size-6"
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </div>
-
-                <div class="relative flex flex-col gap-4 mt-2 flex-1 px-6 py-2">
-                  <a
-                    v-for="item in pages"
-                    :key="item.title"
-                    :href="item.href"
-                    class="block font-semibold leading-6 text-gray-900 mr-10"
-                    :class="{'text-primary': isSelected(item) }"
-                  >
-                    {{ item.title }}
-                  </a>
-                </div>
-
-                <div
-                  v-if="user"
-                  class="flex flex-1 items-end justify-center px-6 py-2"
-                >
-                  <FormButton
-                    method="post"
-                    href="/auth/logout"
-                    text
-                  >
-                    Wyloguj
-                  </FormButton>
-                </div>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
-      </Dialog>
-    </TransitionRoot>
+      <label class="option">
+        <FormButton
+          v-if="user"
+          method="post"
+          href="/auth/logout"
+          text
+        >
+          Wyloguj
+        </FormButton>
+      </label>
+    </div>
   </header>
 </template>
+
+<style scoped>
+.option {
+  @apply py-3 px-4 font-semibold leading-6 text-gray-900 bg-primary/[.02] rounded-xl
+}
+</style>
