bc1d8805c375cd8574c0c45ddf28402a586117ed
Mykyta Mykulskyi <my.garbage813@gmail.com>
2024-12-18 20:07:56 +0100

    feat: change theme for error pages

--- resources/js/components/Crud/CrudSelect.vue
+++ resources/js/components/Crud/CrudSelect.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, defineProps } from 'vue'
+import { ref } from 'vue'
 import { onClickOutside } from '@vueuse/core'
 import vDynamicInputWidth from '@/Helpers/vDynamicInputWidth'
 
--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -13,13 +13,17 @@
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap');
 
+    :root {
+      --theme-color: #E4007D; /* Default color */
+    }
+
     * { margin: 0; padding: 0; }
     html, body { height: 100%; }
     body { display: flex; justify-content: center; align-items: center; font-family: 'Poppins', sans-serif; color: #333; position: relative;  max-width: 48rem; margin:auto }
-    h1 { font-size: 3rem; margin-bottom: 1rem; color: #E4007D; }
+    h1 { font-size: 3rem; margin-bottom: 1rem; color: var(--theme-color); }
     p { font-size: 1rem; margin-bottom: 2rem }
     b { font-size: 1.2rem; margin-bottom: 2rem; font-weight: bold }
-    a { text-decoration: none; background-color: #E4007D; color: white; font-weight: 700; padding: .6rem 1rem; border-radius: .8rem; }
+    a { text-decoration: none; background-color: var(--theme-color); color: white; font-weight: 700; padding: .6rem 1rem; border-radius: .8rem; }
     .container { display: flex; flex-direction: column; align-items: center; text-align: center }
     .bg-overlay { position: fixed; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(255, 255, 255, 0.6); z-index: -10 }
     .blur-background { position: fixed; width: 58.33%; height: 100%; opacity: 0.7; filter: blur(48px); z-index: -10 }
@@ -48,5 +52,10 @@
       <a href="{{ $link['url'] }}">{{ $link['text'] }}</a>
     @endisset
   </div>
+  <script>
+    const theme = localStorage.getItem('theme');
+    let themeColor = theme === 'theme-witelon' ? '#262c89' : '#E4007D';
+    document.documentElement.style.setProperty('--theme-color', themeColor);
+  </script>
 </body>
 </html>
