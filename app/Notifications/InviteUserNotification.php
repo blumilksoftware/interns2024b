761a111d09eb3f34d520af52328567c3d4118d6c
Dominik Purgał <amonsir29@gmail.com>
2024-12-20 11:57:42 +0100

    fix code style

--- app/Notifications/InviteUserNotification.php
+++ app/Notifications/InviteUserNotification.php
@@ -30,7 +30,7 @@ class InviteUserNotification extends Notification implements ShouldQueue
             ->view("emails.auth.invite-user", [
                 "user" => $notifiable,
                 "quiz" => $this->quiz,
-                "url" => url("/dashboard")
+                "url" => url("/dashboard"),
             ]);
     }
 }
