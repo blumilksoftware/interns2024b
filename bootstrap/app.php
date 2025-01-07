782b09e51bfe8fcbde5ff32758227e6e2da5bc65
Dominik Purgał <amonsir29@gmail.com>
2025-01-07 04:50:50 +0100

    add cloudflare support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: "/up",
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: HandleInertiaRequests::class);
+        $middleware->trustProxies(at: "*");
+	$middleware->web(append: HandleInertiaRequests::class);
         $middleware->alias([
             "role" => RoleMiddleware::class,
         ]);
